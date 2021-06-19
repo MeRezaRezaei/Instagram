4f389a6338d20b1fde2a33643a38392db8f3cd77
MeRezaRezaei <77894240+MeRezaRezaei@users.noreply.github.com>
2021-06-19 23:55:41 +0430

    picture migration schema added

--- database/migrations/2021_06_19_174407_create_pictures_table.php
+++ database/migrations/2021_06_19_174407_create_pictures_table.php
@@ -15,6 +15,9 @@ class CreatePicturesTable extends Migration
     {
         Schema::create('pictures', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('post_id')->comment('post id of which this picture belongs to');
+            $table->foreign('post_id')->references('id')->on('posts');
+            $table->string('path',200)->comment('path to the post picture(s)');
             $table->timestamps();
         });
     }
