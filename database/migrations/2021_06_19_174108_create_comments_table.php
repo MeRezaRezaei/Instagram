dbab321c10e1cb376271c6100acfc7dee36cd0fe
MeRezaRezaei <77894240+MeRezaRezaei@users.noreply.github.com>
2021-06-19 23:38:49 +0430

    comments migration schema added

--- database/migrations/2021_06_19_174108_create_comments_table.php
+++ database/migrations/2021_06_19_174108_create_comments_table.php
@@ -15,6 +15,14 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id')->comment('Foreign key to the table users from Comments table');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('post_id')->comment('Foreign key to the table posts from comments table');
+            $table->foreign('user_id')->references('id')->on('posts');
+            $table->unsignedBigInteger('replay_to_id')->comment('Foreign key to the table users from posts table')->nullable();
+            $table->foreign('replay_to_id')->references('id')->on('comments');
+            $table->text('comment')->comment('comment string for this post');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
