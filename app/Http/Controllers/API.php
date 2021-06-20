6689982f1210d0d38ec2999df9fd3cdd2e1c17e3
MeRezaRezaei <77894240+MeRezaRezaei@users.noreply.github.com>
2021-06-20 11:17:41 +0430

    end points added to API cotroller and web route

--- app/Http/Controllers/API.php
+++ app/Http/Controllers/API.php
@@ -6,5 +6,68 @@ use Illuminate\Http\Request;
 
 class API extends Controller
 {
-    //
+    public function registration(){
+        return __FUNCTION__ . ' end point';
+    }
+    public function login(){
+        return __FUNCTION__ . ' end point';
+    }
+    public function new_post(){
+        return __FUNCTION__ . ' end point';
+    }
+    public function new_story(){
+        return __FUNCTION__ . ' end point';
+    }
+    public function send_dm(){
+        return __FUNCTION__ . ' end point';
+    }
+    public function get_profile(){
+        return __FUNCTION__ . ' end point';
+    }
+    public function set_profile(){
+        return __FUNCTION__ . ' end point';
+    }
+    public function send_comment(){
+        return __FUNCTION__ . ' end point';
+    }
+    public function increase_like(){
+        return __FUNCTION__ . ' end point';
+    }
+    public function get_post(){
+        return __FUNCTION__ . ' end point';
+    }
+    public function get_post_feed(){
+        return __FUNCTION__ . ' end point';
+    }
+    public function get_profile_posts(){
+        return __FUNCTION__ . ' end point';
+    }
+    public function get_dialog(){
+        return __FUNCTION__ . ' end point';
+    }
+    public function get_story_feed(){
+        return __FUNCTION__ . ' end point';
+    }
+    public function delete_post(){
+        return __FUNCTION__ . ' end point';
+    }
+    public function follow(){
+        return __FUNCTION__ . ' end point';
+    }
+    public function unfollow(){
+        return __FUNCTION__ . ' end point';
+    }
+    public function delete_comment(){
+        return __FUNCTION__ . ' end point';
+    }
+    public function get_post_comments(){
+        return __FUNCTION__ . ' end point';
+    }
+    public function get_following_lists(){
+        return __FUNCTION__ . ' end point';
+    }
+    public function get_follower_lists(){
+        return __FUNCTION__ . ' end point';
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +15,71 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('registration',
+    [\App\Http\Controllers\API::class,'registration']
+);
+Route::get('login',
+    [\App\Http\Controllers\API::class,'login']
+);
+Route::get('new_post',
+    [\App\Http\Controllers\API::class,'new_post']
+);
+Route::get('new_story',
+    [\App\Http\Controllers\API::class,'new_story']
+);
+Route::get('send_dm',
+    [\App\Http\Controllers\API::class,'send_dm']
+);
+Route::get('get_profile',
+    [\App\Http\Controllers\API::class,'get_profile']
+);
+Route::get('set_profile',
+    [\App\Http\Controllers\API::class,'set_profile']
+);
+Route::get('send_comment',
+    [\App\Http\Controllers\API::class,'send_comment']
+);
+Route::get('increase_like',
+    [\App\Http\Controllers\API::class,'increase_like']
+);
+Route::get('get_post',
+    [\App\Http\Controllers\API::class,'get_post']
+);
+Route::get('get_post_feed',
+    [\App\Http\Controllers\API::class,'get_post_feed']
+);
+Route::get('get_profile_posts',
+    [\App\Http\Controllers\API::class,'get_profile_posts']
+);
+Route::get('get_dialog',
+    [\App\Http\Controllers\API::class,'get_dialog']
+);
+Route::get('get_story_feed',
+    [\App\Http\Controllers\API::class,'get_story_feed']
+);
+Route::get('delete_post',
+    [\App\Http\Controllers\API::class,'delete_post']
+);
+Route::get('follow',
+    [\App\Http\Controllers\API::class,'follow']
+);
+Route::get('unfollow',
+    [\App\Http\Controllers\API::class,'unfollow']
+);
+Route::get('delete_comment',
+    [\App\Http\Controllers\API::class,'delete_comment']
+);
+Route::get('get_post_comments',
+    [\App\Http\Controllers\API::class,'get_post_comments']
+);
+Route::get('get_following_lists',
+    [\App\Http\Controllers\API::class,'get_following_lists']
+);
+Route::get('get_follower_lists',
+    [\App\Http\Controllers\API::class,'get_follower_lists']
+);
+
+
+
+
