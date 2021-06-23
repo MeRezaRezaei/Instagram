188d71263838d1c32ef7d0024a98bffd3bba3f0b
MeRezaRezaei <77894240+MeRezaRezaei@users.noreply.github.com>
2021-06-24 00:07:35 +0430

    soft delete added to post and comments models

--- app/Models/Comments.php
+++ app/Models/Comments.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Comments extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
 
     protected $fillable = [
         'user_id',
@@ -29,7 +30,7 @@ class Comments extends Model
     }
 
     public function replay_child(){
-        return $this->hasOne(Comments::class,'replay_to_id','id');
+        return $this->hasMany(Comments::class,'replay_to_id','id');
     }
 
 
